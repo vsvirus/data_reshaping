d46260f4b476646d5e365351dc6e700da205241a
jsalegre <63122205+jsalegre@users.noreply.github.com>
2020-05-01 15:26:06 +0200

    Add files via upload
    Updated to newest data structure

--- dummy_data.py
+++ dummy_data.py
@@ -15,23 +15,32 @@ boolean = [True,False]
 prev_contitions = ['diabetes','smoker','cancer', 'colesterol']
 parametros = ['uno','dos','tres']
 multipliers = [2,5,10]
-symptoms1 = ['dyspnea','anosmia','ageusia']
+symptoms1 = ['cough','dyspnea','anosmia','ageusia']
 symptoms2 = ['rinorrea','dizziness','nausea']
 symptoms3 = ['diarrhoea','headache','anorexia','articular_pain']
 symptoms4 = ['astenia','chest_pain','palpitations']
+
+complications1 = ['thrombosis','renal_impairment','liver_impairment']
+complications2 = ['arritmia','heart_failure','infection']
+
+lugares = ['Llegada', 'Planta', 'UCI']
+
 blod_preassure = [None,randint(120,160)]
 heart_rate = [None,randint(50,120)]
 respiratory_rate = [None,randint(15,30)]
 o2_saturation_percent = [None,randint(60,100)]
 o2_device_specifier = [None,randint(1,10)]
-pneumonia = [None,randint(0,2)]
+pneumonia = ['unilateral','bilateral','frontal']
+tipo_rx = ['chest_rx_ray','escaner','Tac']
 drogas = ['Morphin', 'Paracetamol', 'Ibuprofeno', 'Amoxicilina', 'Antibioticos']
+cov_test = ['pcr', 'abb', 'ott']
+result = ['positive', 'negative']
 
 
 def dummy_data():
     data = []
     
-    for i in range (100):
+    for i in range (3):
         previous_conditions = [] # done
         test_paciente = [] # done
         clinical_updates = [] # done
@@ -39,7 +48,7 @@ def dummy_data():
         sars_cov2_tests = [] # done
         drugs = [] # done
         
-        n_drugs = randint(0,4)
+        n_drugs = randint(0,2)
         for j in range(n_drugs):
             date = datetime.now()-relativedelta(days=22/(j+1))
             drg = {'_id':str(i), 'date_start':date,
@@ -51,41 +60,35 @@ def dummy_data():
         
         n_sars = randint(0,2)
         for j in range(n_sars):
-            sars = {'_id':str(i), 'timestamp':datetime.now()-relativedelta(days=22/(j+1)),
-                  'result':randint(0,3), 'test_type': randint(0,2)}
+            sars = {'_id':str(i),'timestamp':datetime.now()-relativedelta(days=22/(j+1)),
+                  'result':result[randint(0,1)], 'test_type': cov_test[randint(0,2)]}
             sars_cov2_tests.append(sars)
         
         n_rx = randint(0,2)
         for j in range(n_rx):
-            rx = {'_id':str(i), 'timestamp':datetime.now()-relativedelta(days=22/(j+1)),
-                  'has_pneumonia':boolean[randint(0,1)], 'pneumonia_subtype':pneumonia[randint(0,1)]}
+            rx = {'_id':str(i),'timestamp':datetime.now()-relativedelta(days=22/(j+1)),
+                  'has_pneumonia':boolean[randint(0,1)], 'pneumonia_subtype':pneumonia[randint(0,2)], 'test_type':tipo_rx[randint(0,2)]}
             chest_rxs.append(rx)
         
         
-        n_cu = randint(1,20)
+        n_cu = randint(1,4)
         for j in range(n_cu):
             cu = {'_id': str(i), 'timestamp':datetime.now()-relativedelta(days=22/(j+1)) ,'is_icu_candidate':boolean[randint(0,1)], 
                   'temperature_celsius':randint(35,42), 'blood_pressure_systolic':randint(120,160),
                   'blood_pressure_diastolic':randint(120,160), 'heart_rate': randint(50,120),
-                  'respiratory_rate':randint(15,30),
-                  'o2_saturation_percent':randint(60,100), 'o2_device':randint(0,2),
-                  'o2_device_specifier':randint(1,10),
+                  'respiratory_rate':randint(15,30), "glasgow_coma_scale_score":randint(10,20),
+                  'o2_saturation_percent':randint(60,100), 'o2_device_type':randint(0,2),
+                  'o2_device_model':randint(1,10), 'o2_device_oxygen_concentration_percent':randint(0,100),
+                  'o2_device_flow_liters_per_min':randint(0,4),
                   'has_good_o2_device_adherence':boolean[randint(0,1)], 'needs_prone_position':boolean[randint(0,1)],
-                  'has_symptom_cough':boolean[randint(0,1)], 'has_symptom_dyspena':boolean[randint(0,1)],
-                  'has_symptom_anosmia':boolean[randint(0,1)],'has_symptom_ageusia':boolean[randint(0,1)],
-                  'has_symptom_rinorrea':boolean[randint(0,1)],'has_symptom_dizziness':boolean[randint(0,1)],
-                  'has_symptom_nausea':boolean[randint(0,1)],'has_symptom_diarrhoea':boolean[randint(0,1)],
-                  'has_symptom_headache':boolean[randint(0,1)],'has_symptom_anorexia':boolean[randint(0,1)],
-                  'has_symptom_articular_pain':boolean[randint(0,1)],'has_symptom_astenia':boolean[randint(0,1)],
-                  'has_symptom_chest_pain':boolean[randint(0,1)],'has_symptom_palpitations':boolean[randint(0,1)],
-                  'other_symptoms':'locura', 'has_complication_thrombosis':boolean[randint(0,1)],
-                  'has_complication_renal_impairment':boolean[randint(0,1)],
-                  'has_complication_liver_impairment':boolean[randint(0,1)],'has_complication_arritmia':boolean[randint(0,1)],
-                  'has_complication_heart_failure':boolean[randint(0,1)],'has_complication_infection':boolean[randint(0,1)],
-                  'other_complications':'dierna'}
+                  'symptoms':[symptoms1[randint(0,len(symptoms1)-1)],symptoms2[randint(0,len(symptoms2)-1)],symptoms3[randint(0,len(symptoms3)-1)],symptoms4[randint(0,len(symptoms4)-1)]],
+                  'other_symptoms':['locura'], 
+                  'complications':[complications1[randint(0,len(complications1)-1)],complications2[randint(0,len(complications2)-1)]],
+                  'other_complications':['dierna'],
+                  'location':lugares[randint(0,2)]}
             clinical_updates.append(cu)
         
-        n_tests = randint(1,10)
+        n_tests = randint(1,5)
         for j in range(n_tests): # numero de tests por paciente
             timestamp = j
             raw_text = 'nadaandanaa'
@@ -93,20 +96,20 @@ def dummy_data():
             for k in range(0,3): # numero de parametros por test
                 parameter_key = parametros[k]
                 value = j*70 - 2**j + 100**random()
-                multiplier = multipliers[k]*random()
-                out_of_bounds = False
+                out_of_bounds = [False,True]
                 _id = i*j*k
-                param = {'_id': _id, 'parameter_key': parameter_key, 'value': value, 'multiplier': multiplier, 'out_of_bounds': out_of_bounds}
+                param = {'parameter_key': parameter_key, 'value': value, 'is_out_of_bounds': out_of_bounds[randint(0,1)]}
                 concrete_result.append(param)
-            test = {'concrete_result': concrete_result, '_id': _id, 'timestamp': timestamp, 'raw_text': raw_text}
+            test = {'items': concrete_result, '_id': _id,'timestamp': timestamp, 'text_raw': raw_text}
             test_paciente.append(test)
         
         n_pc = randint(0, 2)
         for j in range(n_pc):
-            pc = {'_id': '5e8f42eb854f7f49880906b6','name': prev_contitions[randint(0,3)],'extra_info': {'insulin_units': 2},'comments': 'bla bla'}
+            pc = {'name': prev_contitions[randint(0,3)],'extra_info': {'insulin_units': 2},'comments': 'bla bla'}
             previous_conditions.append(pc)
     
         patient = {'_id': str(i),
+                   'patient_id':str(i),
                      'health_provider_id': randint(1,3),
                      'gender': randint(1,3),
                      'ethnicity': ethnicity[randint(0,3)],
@@ -117,15 +120,13 @@ def dummy_data():
                      'is_deceased': boolean[randint(0,1)],
                      'height_cm': randint(150, 200),
                      'weight_kg': randint(50, 100),
-                     'last_edition_date': datetime.now(),
-                     'episode_index': randint(1,3),
-                     'previous_contitions': previous_conditions,
-                     'clinical_update': clinical_updates,
-                     'chest_rx': chest_rxs,
-                     'sars_cov2_test': sars_cov2_tests,
+                     'episode_number': randint(1,3),
+                     'previous_conditions': previous_conditions,
+                     'clinical_updates': clinical_updates,
+                     'radiological_tests': chest_rxs,
+                     'coronavirus_tests': sars_cov2_tests,
                      'lab_tests': test_paciente,
-                     'drug': drugs,
-                     '__v': 0}
+                     'prescriptions': drugs}
         
         data.append(patient)
     
